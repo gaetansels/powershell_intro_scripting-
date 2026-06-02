1b3fe34f7a4006b86b7f6a286fb5223db5d2706a
gaetansels <132215754+gaetansels@users.noreply.github.com>
2026-06-02 12:46:14 +0200

    Update Important cmdlets.ps1

--- Important cmdlets.ps1
+++ Important cmdlets.ps1
@@ -1,2 +1,6 @@
-# how to get all the parameters 
+# how to get all the parameters with new-item 
 Get-Help New-Item -Parameter * 
+# how to get all the parameters with get-process 
+Get-Help get-process -Parameter * 
+# how to get all the parameters with get-childitem 
+Get-Help get-childitem -Parameter * 
