d12346539a3922daae97eff39a7e765e6de63f80
gaetansels <132215754+gaetansels@users.noreply.github.com>
2026-06-05 14:06:41 +0200

    Update practice2.ps1

--- practice2.ps1
+++ practice2.ps1
@@ -19,15 +19,19 @@ $mijnauto.merk
 <#
 Opdracht 2: Een eigenschap aanpassen aan een bestaand object
 Je doel:
-Je doel:
-Voeg aan je bestaande $mijnauto object een nieuwe eigenschap Prijs toe met waarde 75000.
-Overschrijf daarna de Kleur van 'Zwart' naar 'Rood' via dot notation.
-Toon tenslotte het volledige object $mijnauto om te controleren of beide wijzigingen zijn doorgevoerd.
+1. Voeg aan je bestaande $mijnauto object een nieuwe eigenschap Prijs toe met waarde 75000.
+2. Overschrijf daarna de Kleur van 'Zwart' naar 'Rood' via dot notation.
+3. Toon tenslotte het volledige object $mijnauto om te controleren of beide wijzigingen zijn doorgevoerd.
 #>
 
 <#
-Hint 1: Nieuwe eigenschap toevoegen werkt met dezelfde dot-notation: $object.NieuweEigenschap = waarde
+💡 Hint 1: Nieuwe eigenschap toevoegen werkt met dezelfde dot-notation: $object.NieuweEigenschap = waarde
 💡 Hint 2: Overschrijven werkt identiek: $object.BestaandeEigenschap = nieuwe_waarde
 💡 Hint 3: Als je het volledige object wilt zien, typ je gewoon $mijnauto (zonder punt erachter).
 Tip: Je mag dit testen in je ISE script door lijnen 18-21 toe te voegen onder je bestaande code, of direct in de console.
 #>
+
+# 1. Voeg aan je bestaande $mijnauto object een nieuwe eigenschap Prijs toe met waarde 75000.
+
+# get help
+get-help Add-Member -Examples
